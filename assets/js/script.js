1b81352598cd1a0f7034249303b0e5abd8e96b11
shishir <shesirsikder1234@gmail.com>
2024-07-02 01:19:08 +0600

    2nd commit

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+let contact_btn = document.getElementById("contact");
+let img_sec = document.querySelector(".imgsec");
+let full_name = document.querySelector(".fullname");
+let sp1 = document.querySelector(".sp1");
+let sp2 = document.querySelector(".sp2");
+
+img_sec.style.display = "none";
+
+contact_btn.addEventListener("click",()=>{
+    contact_btn.style.display = "none";
+    img_sec.style.display = "block";
+    img_sec.style.display = "inline";
+    img_sec.style.display = "flex";
+    
+})
